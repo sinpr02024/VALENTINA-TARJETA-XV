a0d3631c9ce63f4cfde0cb173742d56d2adba901
sinpr02024 <sinproduccionesaud@gmail.com>
2024-07-04 15:42:06 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// script.js
+function updateCountdown() {
+  const targetDate = new Date("July 20, 2024 19:30:00").getTime();
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+
+  if (distance < 0) {
+    document.getElementById("days").textContent = 0;
+    document.getElementById("hours").textContent = 0;
+    document.getElementById("minutes").textContent = 0;
+    document.getElementById("seconds").textContent = 0;
+  }
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
